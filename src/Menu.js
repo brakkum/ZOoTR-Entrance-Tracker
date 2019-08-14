dafbab762bd7494afad169250edca5275e799c8f
Daniel Brakke <brakphoto@gmail.com>
2019-08-13 20:31:25 -0500

    menu to functional component

--- src/Menu.js
+++ src/Menu.js
@@ -1,77 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default class Menu extends React.Component {
+export default ({routeFinderVisible, overworldOnly, ...props}) => {
 
-    state = {
-        message: ""
+    const [message, setMessage] = useState("");
+
+    const saveState = () => {
+        props.saveState();
+        setMessage("Tracker Saved");
     };
 
-    saveState = () => {
-        this.props.saveState();
-        this.flashMessage("Tracker Saved")
-    };
-
-    resetState = () => {
+    const resetState = () => {
         if (!window.confirm("Are you sure you want to reset?")) {
             return;
         }
-        this.props.resetState();
-        this.flashMessage("Tracker Reset");
+        props.resetState();
+        setMessage("Tracker Reset");
     };
 
-    flashMessage = message => {
-        this.setState({message: message});
-        setTimeout(() => {
-            this.setState({message: ""});
-        }, 3000);
-    };
+    useEffect(
+        () => {
+            if (message !== "") {
+                setTimeout(() => {
+                    setMessage("");
+                }, 3000);
+            }
+        }
+    );
 
-    render() {
-        return(
-            <div className="navbar is-fixed-top is-dark">
-                <div style={{width: "100%", maxWidth: "1000px", margin: "auto"}}>
-                    <nav>
-                        <h1 className="is-size-4 navbar-item has-text-white has-text-weight-bold">
-                            ZOoTR Entrance Tracker
-                        </h1>
-                    </nav>
-                    <div className="has-background-dark nav-bottom">
-                        <div className="nav-bottom-left">
-                            <div
-                                className="nav-bottom-item"
-                                onClick={this.props.toggleRouteFinder}
-                            >
-                                {this.props.routeFinderVisible ? "Hide" : "Show"} Route Finder
-                            </div>
-                            <div
-                                className="nav-bottom-item"
-                                onClick={this.props.toggleOverworldOnly}
-                            >
-                                {this.props.overworldOnly ? "Show All Entrances" : "Hide Non-Overworld Entrances"}
-                            </div>
+    return(
+        <div className="navbar is-fixed-top is-dark">
+            <div style={{width: "100%", maxWidth: "1000px", margin: "auto"}}>
+                <nav>
+                    <h1 className="is-size-4 navbar-item has-text-white has-text-weight-bold">
+                        ZOoTR Entrance Tracker
+                    </h1>
+                </nav>
+                <div className="has-background-dark nav-bottom">
+                    <div className="nav-bottom-left">
+                        <div
+                            className="nav-bottom-item"
+                            onClick={props.toggleRouteFinder}
+                        >
+                            {routeFinderVisible ? "Hide" : "Show"} Route Finder
                         </div>
-                        <div className="nav-bottom-right">
-                            <div className="nav-bottom-item has-text-white has-text-weight-bold">
-                                {this.state.message}
-                            </div>
-                            <a href="#save" className="nav-bottom-item has-text-primary" onClick={this.saveState}>
-                                Save
-                            </a>
-                            <a href="#reset" className="nav-bottom-item has-text-light" onClick={this.resetState}>
-                                Reset
-                            </a>
-                            <a
-                                href="https://github.com/brakkum/ZOoTR-Entrance-Tracker"
-                                className="nav-bottom-item has-text-grey-light"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                GitHub
-                            </a>
+                        <div
+                            className="nav-bottom-item"
+                            onClick={props.toggleOverworldOnly}
+                        >
+                            {overworldOnly ? "Show All Entrances" : "Hide Non-Overworld Entrances"}
                         </div>
+                    </div>
+                    <div className="nav-bottom-right">
+                        <div className="nav-bottom-item has-text-white has-text-weight-bold">
+                            {message}
+                        </div>
+                        <a href="#save" className="nav-bottom-item has-text-primary" onClick={saveState}>
+                            Save
+                        </a>
+                        <a href="#reset" className="nav-bottom-item has-text-light" onClick={resetState}>
+                            Reset
+                        </a>
+                        <a
+                            href="https://github.com/brakkum/ZOoTR-Entrance-Tracker"
+                            className="nav-bottom-item has-text-grey-light"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            GitHub
+                        </a>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
