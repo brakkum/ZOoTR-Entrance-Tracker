e031381397391cfba2ec32fc68f8aa20125fb072
Daniel Brakke <brakphoto@gmail.com>
2019-08-25 18:05:15 -0500

    song size update

--- src/Song.js
+++ src/Song.js
@@ -16,7 +16,7 @@ export default function Song({ song, toggleSongCollected }) {
                 ref={hoverRef}
                 onClick={() => toggleSongCollected(song.name)}
             >
-                <h6 className="is-size-6 is-hidden-mobile has-text-weight-semibold">{song.name}</h6>
+                <h6 className="song-name is-hidden-mobile has-text-weight-semibold">{song.name}</h6>
                 <h6 className="is-size-6 is-hidden-desktop is-hidden-tablet">{song.name.split(" ").map(word => word.charAt(0)).join("")}</h6>
             </div>
         </div>
